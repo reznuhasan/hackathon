30b96957b3123cc1bdf36d0c1ca655c5f3c6bf54
its-kayes <kayes.ek8@gmail.com>
2023-09-10 18:33:31 +0600

    Update Home Part

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,33 @@
+import Carousel from 'react-bootstrap/Carousel';
+// import styles from "../styles/edu.module.css"
 import styles from "../styles/home.module.css"
+import HomeSession from './home/HomeSession';
 
 const Home = () => {
   return (
     <div>
-      <section className={styles.mainSection}>
-        <div>
-          <h1> Digital Smart Village </h1>
-          <p> Roadway of Establishing a Comprehensive Digital Smart Village Ecosystem </p>
-        </div>
-        <div>
-          <img className={styles.imgSection} src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg" alt="" />
-        </div>
-      </section>
+      <Carousel fade>
+        <Carousel.Item>
+          <div className={styles.homeCarousel}>
+            <img className={styles.homeCarouselImg} src="/smart-village-2-min.jpg" alt="" />
+          </div>
+          <Carousel.Caption>
+            <h3>Digital Smart Village Ecosystem!</h3>
+            <p>Connecting the Unconnected: Digital Smart Villages</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={styles.homeCarousel}>
+            <img className={styles.homeCarouselImg} src="/smart-village-1-min.jpg" alt="" />
+          </div>
+          <Carousel.Caption>
+            <h3>Connectivity to empower rural communities</h3>
+            <p>Transforming Rural Life: The Digital Smart Village Ecosystem.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <HomeSession />
     </div>
   )
 }
