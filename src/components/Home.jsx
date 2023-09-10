1269b1e09e29c25b6d03092799fe599fa57da86c
its-kayes <kayes.ek8@gmail.com>
2023-09-10 17:19:57 +0600

    Update Home!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import styles from "../styles/home.module.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <section className={styles.mainSection}>
+        <div>
+          <h1> Digital Smart Village </h1>
+          <p> Roadway of Establishing a Comprehensive Digital Smart Village Ecosystem </p>
+        </div>
+        <div>
+          <img className={styles.imgSection} src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg" alt="" />
+        </div>
+      </section>
+    </div>
   )
 }
 
